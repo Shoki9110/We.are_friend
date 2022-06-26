b142fef721b2fdbbc8a88d5c183ed3a5eee1bf6d
syouki <camroad_eleking_193@icloud.com>
2022-06-26 15:53:04 +0900

    Initial Commit

---
+++ We are friend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  We are friend
+//
+//  Created by おのしょうき on 2022/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
