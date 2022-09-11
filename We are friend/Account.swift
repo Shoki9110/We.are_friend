f5e9f9f0873d23b30a37f3c1d97ea63a6704f7ef
syouki <camroad_eleking_193@icloud.com>
2022-09-11 17:24:47 +0900

    Myapp

---
+++ We are friend/Account.swift
@@ -0,0 +1,29 @@
+//
+//  Account.swift
+//  We are friend
+//
+//  Created by おのしょうき on 2022/08/28.
+//
+
+import UIKit
+
+class Account: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ We are friend/AccountmakeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountmakeViewController.swift
+//  We are friend
+//
+//  Created by おのしょうき on 2022/08/28.
+//
+
+import UIKit
+
+class AccountmakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
