2bde24dbe71ef9eeff140845c63a9ed0b306d911
syouki <camroad_eleking_193@icloud.com>
2022-10-23 16:11:19 +0900

    a

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'We are friend' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for We are friend
+
+	pod 'Firebase/Core'
+	pod 'Firebase/Firestore'
+	pod 'Firebase/Auth'
+
+end
--- We are friend/AddViewController.swift
+++ We are friend/AddViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import UIKit
+import Firebase
 
 class AddViewController: UIViewController {
+    
+    @IBOutlet var contentTextView: UITextView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func postContent() {
+        let content = contentTextView.text!
+        let saveDocument = Firestore.firestore().collection("posts").document()
+        saveDocument.setData([
+            "content": content,
+            "postID": saveDocument.documentID,
+            "createdAt": FieldValue.serverTimestamp(),
+            "updatedAt": FieldValue.serverTimestamp()
+        ]) {error in
+            if error == nil {
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+    }
+    func setupTextView() {
+        let toolBar = UIToolbar() // キーボードの上に置くツールバーの生成
+        let flexibleSpaceBarButton = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil) // 今回は、右端にDoneボタンを置きたいので、左に空白を入れる
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissKeyboard)) // Doneボタン
+        toolBar.items = [flexibleSpaceBarButton, doneButton] // ツールバーにボタンを配置
+        toolBar.sizeToFit()
+        contentTextView.inputAccessoryView = toolBar // テキストビューにツールバーをセット
+    }
+
+    // キーボードを閉じる処理。
+    @objc func dismissKeyboard() {
+        contentTextView.resignFirstResponder()
     }
     
 
