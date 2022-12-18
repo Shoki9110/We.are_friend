5afcedda963bb5241b01b4da89bfa70a734b3156
syouki <camroad_eleking_193@icloud.com>
2022-12-18 16:30:36 +0900

    ああ

--- We are friend/ReplyViewController.swift
+++ We are friend/ReplyViewController.swift
@@ -61,65 +61,65 @@
         }
                     
         @objc func keyboardWillShow(_ notification: Notification) {
-                let rect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
-                guard let keyboardHeight = rect?.size.height else { return }
-                let mainBoundsSize = UIScreen.main.bounds.size
-                let textFieldHeight = textField.frame.height
-                
-                let textFieldPositionY = textField.frame.origin.y + textFieldHeight + 10.0
-                let keyboardPositionY = keyboardHeight
-                
-                if keyboardPositionY <= textFieldPositionY {
-                    let duration: TimeInterval? =
-                    notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
-                    UIView.animate(withDuration: duration!) {
-                        self.view.transform = CGAffineTransform(translationX: 0, y: -keyboardPositionY)
-                    }
+            let rect = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            guard let keyboardHeight = rect?.size.height else { return }
+            let mainBoundsSize = UIScreen.main.bounds.size
+            let textFieldHeight = textField.frame.height
+            
+            let textFieldPositionY = textField.frame.origin.y + textFieldHeight + 10.0
+            let keyboardPositionY = keyboardHeight
+            
+            if keyboardPositionY <= textFieldPositionY {
+                let duration: TimeInterval? =
+                notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double
+                UIView.animate(withDuration: duration!) {
+                    self.view.transform = CGAffineTransform(translationX: 0, y: -keyboardPositionY)
                 }
-            }
-                    
-            // キーボード非表示通知の際の処理
-            @objc func keyboardWillHide(_ notification: Notification) {
-                let duration: TimeInterval? = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
-                UIView.animate(withDuration: duration ?? 0, delay: 0, options: .curveEaseOut) {
-                    self.view.transform = CGAffineTransform.identity
-                }
-            }
-            @IBAction func pushBotton(_ sender: UIButton) {
-                
-                textFieldString = textField.text!
-                print(textFieldString)
-                print("a")
-                
-                textField.text = ""
-                
-//              commentArray.append(textFieldString)
-                let content = textFieldString
-                let saveDocument = Firestore.firestore().collection("comments").document()
-                saveDocument.setData([
-                    "content": content,
-                    "postID": selectedID,
-                    "commentID": saveDocument.documentID,
-                    "createdAt": FieldValue.serverTimestamp(),
-                ]) {error in
-                    if error == nil {
-                    }
-                }
-                table.reloadData()
-            }
-            override func didReceiveMemoryWarning() {
-                super.didReceiveMemoryWarning()
-            }
-            func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-                return commentArray.count
-            }
-            func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-                let cell = table.dequeueReusableCell(withIdentifier: "Cell")
-                
-                cell?.textLabel?.text = commentArray[indexPath.row].content
-                return cell!
-            }
-            func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-                print("\(commentArray[indexPath.row])が選ばれました！")
             }
         }
+                    
+        // キーボード非表示通知の際の処理
+        @objc func keyboardWillHide(_ notification: Notification) {
+            let duration: TimeInterval? = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? Double
+            UIView.animate(withDuration: duration ?? 0, delay: 0, options: .curveEaseOut) {
+                self.view.transform = CGAffineTransform.identity
+            }
+        }
+        @IBAction func pushBotton(_ sender: UIButton) {
+            
+            textFieldString = textField.text!
+            print(textFieldString)
+            print("a")
+            
+            textField.text = ""
+            
+//              commentArray.append(textFieldString)
+            let content = textFieldString
+            let saveDocument = Firestore.firestore().collection("comments").document()
+            saveDocument.setData([
+                "content": content,
+                "postID": selectedID,
+                "commentID": saveDocument.documentID,
+                "createdAt": FieldValue.serverTimestamp(),
+            ]) {error in
+                if error == nil {
+                }
+            }
+            table.reloadData()
+        }
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+        }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return commentArray.count
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = table.dequeueReusableCell(withIdentifier: "Cell")
+            
+            cell?.textLabel?.text = commentArray[indexPath.row].content
+            return cell!
+        }
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            print("\(commentArray[indexPath.row])が選ばれました！")
+        }
+}
