58387c060db829c81c8f940fb92972994f9cf2ba
syouki <camroad_eleking_193@icloud.com>
2023-02-05 12:25:04 +0900

    不適切だと思った投稿をgoogleフォームに飛ばす Tab ber を追加した

---
+++ We are friend/TabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TabBarViewController.swift
+//  We are friend
+//
+//  Created by おのしょうき on 2023/02/05.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
